6aa02afed84f3300cfec31a417d7db3436602f44
xidddekate <77233279+xidddekate@users.noreply.github.com>
2021-01-14 01:06:04 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,9 +1,11 @@
+# Code can be used with assistant.py to send mails
+
 import smtplib
 def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('sudhanshudekate7@gmail.com', 'pass')
-    server.sendmail('sudhanshudekate7@gmail.com', to, content)
+    server.login('XYZ@gmail.com', 'pass')
+    server.sendmail('XYZ@gmail.com', to, content)
     server.close()
-sendEmail('sudhanshudekate8@gmail.com',"this is test mail")
+sendEmail('XYZ@gmail.com',"this is test mail")
